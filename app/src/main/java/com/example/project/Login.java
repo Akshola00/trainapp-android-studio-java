b7e5239817bdfe0086d73acc6bfcb8745ed6a904
Akshola00 <akinniyishola07@gmail.com>
2024-05-06 23:30:43 +0100

    implementing auth

--- app/src/main/java/com/example/project/Login.java
+++ app/src/main/java/com/example/project/Login.java
@@ -2,13 +2,36 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+    String login_email;
+    String login_password;
+
+    TextView login_signuplink;
+    Button login_login, login_continuewithgooogle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_email = ((EditText)findViewById(R.id.login_email)).getText().toString();
+        login_password = ((EditText)findViewById(R.id.login_password)).getText().toString();
+        login_signuplink = findViewById(R.id.login_signuplink);
+
+        login_signuplink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
