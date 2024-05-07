9c50594d4b44bb9fe8377b004977b4a7347d3e83
Akshola00 <akinniyishola07@gmail.com>
2024-05-07 14:15:19 +0100

    implementing auth

--- app/src/main/java/com/example/project/accountFragment.java
+++ app/src/main/java/com/example/project/accountFragment.java
@@ -1,12 +1,21 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +31,10 @@ public class accountFragment extends Fragment {
 
     // TODO: Rename and change types of parameters
     private String mParam1;
+    TextView accountfrag_logout, accountfrag_username;
+    FirebaseAuth auth;
+    FirebaseUser user;
+
     private String mParam2;
 
     public accountFragment() {
@@ -53,6 +66,8 @@ public class accountFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
@@ -60,5 +75,33 @@ public class accountFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+        accountfrag_logout = view.findViewById(R.id.accountfrag_logout);
+        accountfrag_username = view.findViewById(R.id.accountfrag_username);
+        user = auth.getCurrentUser();
+        if (user == null){
+            Toast.makeText(getActivity(), "Please Login to access Home page", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getActivity(), Login.class);
+            startActivity(intent);
+            requireActivity().finish();
+        } else {
+            accountfrag_username.setText("Welcome" + user.getEmail());
+        }
+        accountfrag_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(getActivity(), "SUCCESSFULLY SIGNED OUT", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+                requireActivity().finish();
+            }
+        });
     }
 }
